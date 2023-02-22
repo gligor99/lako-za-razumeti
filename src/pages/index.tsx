d3cbf30c827a34e4fc76354081187d86320d93bf
luka <lukagligorevic@gmail.com>
2023-02-22 14:36:49 +0100

    max_tokens updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       model: 'text-davinci-003',
       prompt: prompt,
       temperature: 0.7,
-      max_tokens: 2048,
+      max_tokens: 256,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0,
